bf62d59a33e1d84c42c620b191c3a28dd91ed8c5
Qingkaz <114265166+Qingkaz@users.noreply.github.com>
2023-10-25 17:45:06 -0700

    Upload StockApp

---
+++ StockApp/Utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  StockApp
+//
+//  Created by 青单 on 10/21/23.
+//
+
+import Foundation
+
+let apiKey = "e69f3b18bd467dcd6bf94f62822b344d"
+let baseURL = "https://financialmodelingprep.com/api/v3/"
