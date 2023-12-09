da21215d0ce432bfcbd9730bb4ed5649404de8e5
Qing Shan <shan.qi@northeastern.edu>
2023-12-09 01:50:48 -0800

    Upload NamePhoneNumberProtocalDelegate

---
+++ NamePhoneNumberProtocalDelegate/NamePhoneNumberProtocalDelegate/SendNamePhoneNumberDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SendNamePhoneNumberDelegate.swift
+//  NamePhoneNumberProtocalDelegate
+//
+//  Created by 青单 on 12/9/23.
+//
+
+import Foundation
+
+protocol SendNamePhoneNumberDelegate{
+    func sendNamePhoneNumber(name: String, phoneNumber: String)
+}
